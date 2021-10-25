25d4ed67b0ffa7a0a8dd1b559f07d2e104efb543
Matteo Miceli <omatteomiceli@gmail.com>
2021-10-25 10:48:48 -0700

    updated background and some other minor changes

---
+++ index.js
@@ -0,0 +1,7 @@
+// let githubArr = document.querySelectorAll('.anchor-github');
+
+// githubArr.forEach(github => {
+//     github.addEventListener('mouseenter', function (e) {
+//         e.target.children[0].attributes.src = './icons/github.svg'
+//     });
+// });
